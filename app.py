586f061ea012aa3131aff3e30115ac5733c75865
sty <1455670697@qq.com>
2021-06-29 16:45:01 +0800

    增加万年历

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import pandas as pd
 import charges.charges as charges
 import computer.computer as computer
 import triangle.triangle as triangle
+import thecalendar.thecalendar as thecalendar
 from flask_cors import CORS
 from myutils import *
 
@@ -35,7 +36,6 @@ def question1():
     da = json.dumps(df.to_dict(orient='records'))
 
     response = make_response(da)
-    response.headers['Access-Control-Allow-Origin'] = "*"
 
     return response
 
@@ -57,7 +57,6 @@ def question2():
     da = json.dumps(df.to_dict(orient='records'))
 
     response = make_response(da)
-    response.headers['Access-Control-Allow-Origin'] = "*"
 
     return response
 
@@ -79,7 +78,27 @@ def question3():
     da = json.dumps(df.to_dict(orient='records'))
 
     response = make_response(da)
-    response.headers['Access-Control-Allow-Origin'] = "*"
+
+    return response
+
+
+@app.route('/question4', methods=['POST', 'GET'])
+def question4():
+    file = request.files['file']
+    file.save(os.getcwd() + '/' + file.filename)
+    df = pd.read_csv(file.filename, sep=',', header=None)
+    df[5] = 0
+    df[6] = 0
+    for i in range(df.shape[0]):
+        df.loc[i, 5] = thecalendar.calendar_atom([df[1][i], df[2][i], df[3][i]])
+        if str(df[4][i]) != str(df[5][i]):
+            df.loc[i, 6] = "未通过测试"
+        else:
+            df.loc[i, 6] = "通过测试"
+
+    da = json.dumps(df.to_dict(orient='records'))
+
+    response = make_response(da)
 
     return response
 
